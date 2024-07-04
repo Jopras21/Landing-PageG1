3971f8e9a055800e4799078caafd2fb897750583
Jopras21 <sun.phin098@gmail.com>
2024-07-04 13:54:35 +0700

    delete

---
+++ src/app/header/Header.tsx
@@ -0,0 +1,10 @@
+import './header.css';
+import {Text} from '../core-ui/text/Text';
+
+export default function Header() {
+  return (
+    <header>
+      <Text size="header" title="G1 Brand Advertising" />
+    </header>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import './footer/footer.css';
+import Navbar from './core-ui/footer/Navbar';
+export default function Home() {
+  return (
+    <main>
+      <Footer />
+    </main>
+  );
+}
