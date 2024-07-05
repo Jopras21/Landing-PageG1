7568c49cc66202370b453a3ef2e08976d6310385
Jopras21 <sun.phin098@gmail.com>
2024-07-05 14:55:05 +0700

    component update our services with it responsivenes, text, and images

--- src/app/scenes/OurServices.tsx
+++ src/app/scenes/OurServices.tsx
@@ -8,38 +8,66 @@ export default function OurServices() {
       <div className="service-container">
         <div className="service">
           <div className="services-img">
-            <img src="" alt="services" />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/4961/4961639.png"
+              alt="Instalasi & Pemasangan"
+            />
           </div>
           <div className="service-text">
-            <Text size="normal" title="Service" />
-            <Text size="small" title="Deskripsi Service 1" />
+            <Text size="normal" title="Instalasi & Pemasangan" />
+            <Text
+              size="small"
+              title="Kami menawarkan instalasi dan pemasangan yang mudah dan cepat."
+              style={{marginTop: '.3rem'}}
+            />
           </div>
         </div>
         <div className="service">
           <div className="services-img">
-            <img src="" alt="services" />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2921/2921222.png"
+              alt="Pemeliharaan & Dukungan Teknis"
+            />
           </div>
           <div className="service-text">
-            <Text size="normal" title="Service" />
-            <Text size="small" title="Deskripsi Service 2" />
+            <Text size="normal" title="Pemeliharaan & Dukungan Teknis" />
+            <Text
+              size="small"
+              title="Kami menyediakan pemeliharaan dan dukungan teknis agar sistem berjalan optimal"
+              style={{marginTop: '.3rem'}}
+            />
           </div>
         </div>
         <div className="service">
           <div className="services-img">
-            <img src="" alt="services" />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2936/2936704.png"
+              alt="Layanan Pembuatan Iklan"
+            />
           </div>
           <div className="service-text">
-            <Text size="normal" title="Service" />
-            <Text size="small" title="Deskripsi Service 3" />
+            <Text size="normal" title="Layanan Pembuatan Iklan" />
+            <Text
+              size="small"
+              title="Kami membantu Anda membuat iklan yang menarik untuk mencapai target audiens."
+              style={{marginTop: '.3rem'}}
+            />
           </div>
         </div>
         <div className="service">
           <div className="services-img">
-            <img src="" alt="services" />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2867/2867142.png"
+              alt="Penargetan Lokasi"
+            />
           </div>
           <div className="service-text">
-            <Text size="normal" title="Service" />
-            <Text size="small" title="Deskripsi Service 4" />
+            <Text size="normal" title="Penargetan Lokasi" />
+            <Text
+              size="small"
+              title="Kami menyediakan layanan penargetan lokasi untuk memastikan produk anda tersalurkan secara tepat"
+              style={{marginTop: '.3rem'}}
+            />
           </div>
         </div>
       </div>
