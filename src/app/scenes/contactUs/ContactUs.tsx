edeba797067f76de29c501b6a2921f068f7542c2
Jopras21 <sun.phin098@gmail.com>
2024-07-10 10:38:32 +0700

    scenes contact us making contact us button with its responsive

---
+++ src/app/scenes/contactUs/ContactUs.tsx
@@ -0,0 +1,10 @@
+import './contactUs.css';
+import Button from '@/app/core-ui/button/Button';
+
+export default function ContactUs() {
+  return (
+    <div className="contact-us">
+      <Button type='primary' title='&#9993; gtrone@grosir.one' className='contact-us-button'/>
+    </div>
+  );
+}
