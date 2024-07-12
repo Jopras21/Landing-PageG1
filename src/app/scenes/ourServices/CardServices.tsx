908223bd73555a2a546493e2b8f6a0f993229ae3
Jopras21 <sun.phin098@gmail.com>
2024-07-12 15:52:01 +0700

    scenes our services card styling and cursor bubble, on progress

--- src/app/scenes/ourServices/CardServices.tsx
+++ src/app/scenes/ourServices/CardServices.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 import './ourServices.css';
-import {Text} from '@/app/core-ui/text/Text';
+import { Text } from '@/app/core-ui/text/Text';
 
 interface CardProps {
   imageSrc: string;
@@ -8,15 +10,62 @@ interface CardProps {
   description: string;
 }
 
-const CardServices: React.FC<CardProps> = ({imageSrc, subtitle, description}) => {
+const CardServices: React.FC<CardProps> = ({
+  imageSrc,
+  subtitle,
+  description,
+}) => {
+  useEffect(() => {
+    const cards = document.querySelectorAll('.service');
+    cards.forEach((card) => {
+      const bubbles = card.querySelectorAll('.bubble');
+      card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        bubbles.forEach((bubble) => {
+          bubble.style.left = `${x}px`;
+          bubble.style.top = `${y}px`;
+        });
+        card.classList.add('hovered');
+        card.classList.remove('blur');
+        cards.forEach((otherCard) => {
+          if (otherCard !== card) {
+            otherCard.classList.add('blur');
+          }
+        });
+      });
+
+      card.addEventListener('mouseleave', () => {
+        card.classList.remove('hovered');
+        cards.forEach((otherCard) => {
+          otherCard.classList.remove('blur');
+        });
+      });
+    });
+  }, []);
+
   return (
     <div className="service">
+      {[...Array(5)].map((_, i) => (
+        <div
+          key={i}
+          className={`bubble bubble-${i}`}
+          style={{ animationDuration: `${3 + i * 2}s` }}
+        ></div>
+      ))}
       <div className="services-img">
         <img src={imageSrc} alt="Service" />
       </div>
       <div className="service-text">
         <Text size="normal" title={subtitle} />
-        <Text size="small" title={description} style={{marginTop: '.3rem'}} />
+        <hr className="divider" />
+        <Text
+          className="description"
+          size="small"
+          title={description}
+          style={{ marginTop: '.3rem' }}
+        />
       </div>
     </div>
   );
